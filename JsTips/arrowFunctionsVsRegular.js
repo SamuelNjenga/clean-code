7dc1beb0a82d0c431140ce6f92b564ff8d9fb137
Samuel Njenga <snjenga704@gmail.com>
2020-06-15 20:30:34 +0300

    Added arrowFunctionsVsRegular.js

---
+++ JsTips/arrowFunctionsVsRegular.js
@@ -0,0 +1,29 @@
+const hello = (name) => {
+    console.log("Hello!", name);
+}
+hello('Sam Njenga');
+
+hi = (name) => {
+    console.log("Hi!", name);
+}
+
+hi('Nice Programmer');
+
+
+function dev(name) {
+    console.log(name)
+}
+dev('Bill Gates')
+
+const printName = function (firstName, lastName) {
+    const name = firstName + lastName;
+    console.log(name);
+}
+printName('Kelvin ', 'Kimaru')
+
+const city = function fullCity(firstName, lastName) {
+    const city = firstName + lastName;
+    console.log(city);
+}
+city('Ruiru ', 'Kiambu')
+
