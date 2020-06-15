2505abf951f0e55ec9c8cd1e035b4ded981b60ad
Samuel Njenga <snjenga704@gmail.com>
2020-06-16 00:14:05 +0300

    Added newTester.js

---
+++ JsTips/newTester.js
@@ -0,0 +1,36 @@
+const alphabets = ["a", "b", "c"];
+alphabets.length = 0;
+console.log(alphabets);
+
+const a = 5;
+const b = 10;
+
+console.log(`The result is : ${a+b}!`)
+
+let str = "Lets code with Samuel."
+let res = str.split(" ");
+console.log(res[0] + " " + res[1]);
+console.log(res[2] + res[3]);
+
+let str2 = "Samuel"
+let res2 = str2.split(" ");
+console.log(res[0]);
+
+const arr = [];
+console.log(Array.isArray(arr))
+
+const arr2 = {};
+console.log(Array.isArray(arr2))
+
+const arr3 = [];
+console.log(typeof (arr3))
+
+
+function foo() {
+    let a = 0,b;
+    a++;
+    return a;
+}
+console.log(foo())
+
+console.log(typeof(b))
