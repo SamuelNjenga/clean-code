cdbc1d98b51d5841f9b502bc76761c5a975cc330
Samuel Njenga <snjenga704@gmail.com>
2020-06-15 20:14:51 +0300

    Added numStringsHacks.js

---
+++ JsTips/numStringsHacks.js
@@ -0,0 +1,15 @@
+console.log("2" + "5"); //25
+console.log("4" + "5" + "2"); //452
+console.log("56" - "4"); // 52
+console.log(typeof("34" - "4")); //number
+console.log(typeof("name")); //String
+console.log(typeof("4")); //String
+
+const sum = eval("15* 15 +10");
+console.log(sum);
+
+console.log(String("Ravin") === "Ravin")
+console.log(String("123") === "123")
+console.log(Number("100") === "100")
+console.log(Number("100") === 100)
+console.log(Number(100) === 100)
