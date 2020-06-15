f5825b6910a8ee42d15247cfb120bf38e7d50328
Samuel Njenga <snjenga704@gmail.com>
2020-06-15 19:54:16 +0300

    Added numberFunctions.js

---
+++ JsTips/numberFunctions.js
@@ -0,0 +1,26 @@
+// const num = parseInt('100')
+// const num1 = parseFloat('100.01')
+
+// console.log(num);
+// console.log(num1);
+
+// console.log(num * 5);
+// console.log(num1 / 4);
+
+// const num = Number('100');
+// const num1  = Number('100.01');
+
+// console.log(num);
+// console.log(num1);
+
+// console.log(num * 5);
+// console.log(num1 / 4);
+
+// const num = +'100';
+// const num1  = +'100.01';
+
+// console.log(num);
+// console.log(num1);
+
+// console.log(num * 5);
+// console.log(num1 / 4);
