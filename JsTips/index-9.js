6b61600723b657225d336e678558289e7634536b
Samuel Njenga <snjenga704@gmail.com>
2020-07-20 22:30:41 +0300

    Added index-9.js

---
+++ JsTips/index-9.js
@@ -0,0 +1,17 @@
+const arr = [3,6,12,5]
+const mod = arr.fill(2,90);
+console.log(arr);
+console.log(mod);
+
+console.log(typeof NaN);
+console.log(typeof Number);
+
+console.log([123,456,567].slice(1));
+console.log([123,456,567].slice(2));
+console.log([123,456,567].slice(0));
+console.log([123,456,567].slice(0,-1));
+
+console.log([] == ![])
+
+let n = 'hello'
+console.log(typeof +n)
