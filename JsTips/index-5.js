6c90d1dc2fe8a208d55c19b51aed957fac82ce9c
Samuel Njenga <snjenga704@gmail.com>
2020-07-20 21:54:08 +0300

    Added index-5.js in my JsTips directory

---
+++ JsTips/index-5.js
@@ -0,0 +1,14 @@
+//Accessing object properties dynamically
+const twitList = {
+    'Samuel' : 3002,
+    'Kevin' : 2002,
+    'Ann' :4002
+}
+
+const nameOne = 'Samuel';//name can be dynamic meaning it can be changed
+const followersA = twitList[nameOne];//Using square bracket notation[], key can be passed from variable and respective value retrieved
+console.log(`${nameOne} has ${followersA} followers`);
+
+const nameTwo = 'Ann';
+const followersB = twitList[nameTwo];
+console.log(`${nameTwo} has ${followersB} followers`);
